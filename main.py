aa5193ca5e765b445cc4c63693ff3a87f8da3274
Jared Barrow <kjaredb96@gmail.com>
2023-06-11 19:50:16 -0400

    Add Generator template
    Add initial template for simple generator function.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+def generate_text(prompt: str, model) -> str:
+    return ''
+
+
 if __name__ == '__main__':
-    print('Hello world!')
+    input_prompt = 'Hello.'
+    gpt_model = None
+    print(generate_text(input_prompt, gpt_model))
